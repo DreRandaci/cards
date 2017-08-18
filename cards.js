cf86a952693b05adcc76618b943057144a297f1a
Dre mf Randaci <drerandaci@gmail.com>
2017-08-17 20:36:19 -0500

    finished js, started basic styles

--- cards.js
+++ cards.js
@@ -1,55 +1,52 @@
 
-let cardsArray = [
-{	
-	thing: 'A full comprehension of the Javascript',
-	url: 'https://static.superdeluxe.com/dankland/generators/when-you-try-to-read-the-.jpg',
-}, {	
-	thing: 'Time and flexibility to finish the assignments',
-	url: 'https://www.rcthomas.com/wp-content/uploads/2016/08/iStock_52225964_XLARGE-1024x683.jpg'
-}, {	
-	thing: "Encouragement from your class when you finally figure CSS and your website looks like it's from 2002",
-	url: 'https://s-media-cache-ak0.pinimg.com/originals/cb/26/e5/cb26e51d88d1a0787e580c1c624f7e46.jpg',
-}]; //{	
-// 	thing: '',
-// 	url:
+// let cardsArray = [
+// {	
+// 	thing: 'A full comprehension of the Javascript',
+// 	url: 'https://static.superdeluxe.com/dankland/generators/when-you-try-to-read-the-.jpg',
 // }, {	
-// 	thing:,
-// 	url:
+// 	thing: 'Time and flexibility to finish the assignments',
+// 	url: 'https://www.rcthomas.com/wp-content/uploads/2016/08/iStock_52225964_XLARGE-1024x683.jpg'
+// }, {	
+// 	thing: "Encouragement from your class when you finally configure CSS and your website looks like it's from 2002",
+// 	url: 'https://s-media-cache-ak0.pinimg.com/originals/cb/26/e5/cb26e51d88d1a0787e580c1c624f7e46.jpg',
+// }, {	
+//  	thing: 'Money For $10 Dollar Brunch Beers',
+// 	url: 'http://www.nateleung.com/assets/7-Reasons-why-you%E2%80%99re-Dumb-and-Broke.jpg'
 // }];
 
 let cardsContainer = document.getElementById('cardsContainer');
-let textArea = document.getElementById('textArea');
 let createBtn = document.getElementById('createBtn');
-let deleteBtn = document.getElementById('deleteBtn');
-let thing = document.getElementById('thing');
 
-function createCard(cards) {
-	let card = '';
+function createCard() {
+	let textInput = document.getElementById('textInput').value;
 	let newCard = '';
-	//may not need the loop
-	//for (let i=0;i<cardsArray.length;i++){
 		newCard += `<div id='cardBox'>`;
-		newCard += `<div id='thing'>${cards[0].thing}</div>`;
-		newCard += `<img class="thingImage" src="${cards[0].url}">`;
+		newCard += `<div class='text'><h3>${textInput}</h3></div>`;
+		newCard += `<div class='deleteBtnDiv'>`;
+		newCard += `<button class='deleteBtn'>Delete</button>`;
+		newCard += `<div>`;
 		newCard += `</div>`;
-		newCard += `<button id='deleteBtn'>Delete</button>`;
-		card += newCard;
-	//};
-	writeCardToDom(card);
+	writeCardToDom(newCard);
 };
 
 function writeCardToDom(card) {
-	cardsContainer.innerHTML = card;
-}
+	cardsContainer.innerHTML += card;
+};
+
+function deleteCard(event) {
+	event.target.parentNode.parentNode.remove();
+	event.target.remove();
+};
 
 createBtn.addEventListener('click', function(){
-	createCard(cardsArray);
-})
+	createCard();
+});
 
-// deleteBtn.addEventListener('click', function(){
-// 	cardsContainer.remove('thing');
-// })
-
+document.body.addEventListener('click', function(event){
+  if(event.target.className === 'deleteBtn') {
+    deleteCard(event);
+  };
+});
 
 
 
