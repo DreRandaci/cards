b515794d081542ed04c50491ece8d50632faf803
Dre mf Randaci <drerandaci@gmail.com>
2017-08-17 23:22:44 -0500

    finished basic styles and functionality

--- cards.js
+++ cards.js
@@ -1,30 +1,17 @@
 
-// let cardsArray = [
-// {	
-// 	thing: 'A full comprehension of the Javascript',
-// 	url: 'https://static.superdeluxe.com/dankland/generators/when-you-try-to-read-the-.jpg',
-// }, {	
-// 	thing: 'Time and flexibility to finish the assignments',
-// 	url: 'https://www.rcthomas.com/wp-content/uploads/2016/08/iStock_52225964_XLARGE-1024x683.jpg'
-// }, {	
-// 	thing: "Encouragement from your class when you finally configure CSS and your website looks like it's from 2002",
-// 	url: 'https://s-media-cache-ak0.pinimg.com/originals/cb/26/e5/cb26e51d88d1a0787e580c1c624f7e46.jpg',
-// }, {	
-//  	thing: 'Money For $10 Dollar Brunch Beers',
-// 	url: 'http://www.nateleung.com/assets/7-Reasons-why-you%E2%80%99re-Dumb-and-Broke.jpg'
-// }];
-
 let cardsContainer = document.getElementById('cardsContainer');
 let createBtn = document.getElementById('createBtn');
+let clearBtn = document.getElementById('clearBtn');
+let textInput = document.getElementById('textInput');
 
 function createCard() {
-	let textInput = document.getElementById('textInput').value;
+	let textInputValue = document.getElementById('textInput').value;
 	let newCard = '';
 		newCard += `<div id='cardBox'>`;
-		newCard += `<div class='text'><h3>${textInput}</h3></div>`;
+		newCard += `<div class='text'><h3>${textInputValue}</h3></div>`;
 		newCard += `<div class='deleteBtnDiv'>`;
-		newCard += `<button class='deleteBtn'>Delete</button>`;
-		newCard += `<div>`;
+		newCard += `<button class='deleteBtn' btn>Delete</button>`;
+		newCard += `</div>`;
 		newCard += `</div>`;
 	writeCardToDom(newCard);
 };
@@ -48,5 +35,15 @@ document.body.addEventListener('click', function(event){
   };
 });
 
+textInput.addEventListener('keypress', function(event){
+	if (event.key === 'Enter') {
+  		createCard();
+	};
+});
+
+clearBtn.addEventListener('click', function() {
+	cardsContainer.innerHTML = '';
+	textInput.value = '';
+});
 
 
